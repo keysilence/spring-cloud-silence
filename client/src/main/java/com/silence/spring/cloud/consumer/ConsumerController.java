dea82ab71dc912e56809fcf39f34b4f517ce7381
Silence <chenmohaha_2000@163.com>
2018-05-07 10:07:48 +0800

    first commit

--- client/src/main/java/com/silence/spring/cloud/consumer/ConsumerController.java
+++ client/src/main/java/com/silence/spring/cloud/consumer/ConsumerController.java
@@ -1,7 +1,6 @@
 package com.silence.spring.cloud.consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
---
+++ zuul/src/main/java/com/silence/spring/cloud/zuul/ZuulApplication.java
@@ -0,0 +1,22 @@
+package com.silence.spring.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by Silence on 2018/5/4.
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZuulApplication.class, args);
+
+    }
+
+}
