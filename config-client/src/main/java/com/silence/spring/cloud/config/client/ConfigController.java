8b1ef1b70c481b1430918d288194f04a00f2032d
Silence <chenmohaha_2000@163.com>
2019-08-08 13:24:50 +0800

    完善了ConfigServer

--- config-client/src/main/java/com/silence/spring/cloud/config/client/ConfigController.java
+++ config-client/src/main/java/com/silence/spring/cloud/config/client/ConfigController.java
@@ -1,6 +1,7 @@
 package com.silence.spring.cloud.config.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by Silence on 2018/5/7.
  */
 @RestController
+@RefreshScope
 public class ConfigController {
 
-    @Value("${test.hello}")
+    @Value("${group.00}")
     private String testString;
 
     @RequestMapping("/test")
